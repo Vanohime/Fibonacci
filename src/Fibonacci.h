9428fff152c1b61d79e0cb2436919ea1a4d1fafe
Ivan Harohau <olevel806@gmail.com>
2025-02-12 21:00:58 +0300

    Добавьте файлы проекта.

---
+++ src/Fibonacci.h
@@ -0,0 +1,21 @@
+﻿#ifndef FIBONACCI_H
+#define FIBONACCI_H
+#pragma once
+#include <vector>
+
+class FibonacciCalculator {
+public:
+    virtual unsigned long calculate(int n) const = 0;  
+};
+
+class Fibonacci : public FibonacciCalculator {
+public:
+    Fibonacci();
+    unsigned long calculate(int n) const override;
+    std::vector<unsigned long> getSequence(int n) const;
+
+private:
+    unsigned long fib(int n) const;
+};
+
+#endif // FIBONACCI_H
